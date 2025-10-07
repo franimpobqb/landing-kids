514992e17d365ac2cdcd983cb7705e68f4d69387
franimpo <fimpollino@buquebus.com>
2025-10-07 12:56:44 -0300

    menu

--- js/scripts-kids.js
+++ js/scripts-kids.js
@@ -1,3 +1,9 @@
+const toggler = document.getElementById("menu-toggler");
+const body = document.body;
+
+toggler.addEventListener("click", () => {
+  body.classList.toggle("menu-open");
+});
 
 // Init thumbs swiper
 const swiperThumbs = new Swiper(".swiperThumbs", {
@@ -11,35 +17,35 @@ const swiperMain = new Swiper(".swiperMain", {
   breakpoints: {
     640: {
       slidesPerView: 1.5,
-      spaceBetween: 80,
+      spaceBetween: 20,
     },
     1024: {
       slidesPerView: 1.2,
       spaceBetween: 50,
       centeredSlides: true,
- coverflowEffect: {
-     rotate: 0,
-     stretch: 800,
-     depth: 350,
-     modifier: 1,
-     slideShadows: false,
-   }, 
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 800,
+        depth: 350,
+        modifier: 1,
+        slideShadows: false,
+      },
     },
   },
   effect: "coverflow",
- 
   loop: true,
   grabCursor: true,
   centeredSlides: true,
-slidesPerView: 1.1,
+  slidesPerView: 1.05,
+     spaceBetween: 20,
   on: {
     slideChange: function () {
       const slides = this.slides;
       slides.forEach((slide, index) => {
-        const video = slide.querySelector('video');
+        const video = slide.querySelector("video");
         if (video) {
           if (index === this.activeIndex) {
-            video.play().catch(() => { }); // autoplay might need muted
+            video.play().catch(() => {}); // autoplay might need muted
           } else {
             video.pause();
             video.currentTime = 0; // optional, restart when inactive
@@ -51,11 +57,12 @@ slidesPerView: 1.1,
   navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
   keyboard: { enabled: true },
   mousewheel: { forceToAxis: true },
-  thumbs: { swiper: swiperThumbs }
+  thumbs: { swiper: swiperThumbs },
 });
 
 // Start first slide’s video automatically
-window.addEventListener('load', () => {
-  const firstVideo = swiperMain.slides[swiperMain.activeIndex].querySelector('video');
-  if (firstVideo) firstVideo.play().catch(() => { });
+window.addEventListener("load", () => {
+  const firstVideo =
+    swiperMain.slides[swiperMain.activeIndex].querySelector("video");
+  if (firstVideo) firstVideo.play().catch(() => {});
 });
